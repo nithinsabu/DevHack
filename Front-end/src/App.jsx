5999ef3e10013526f9d2bcdff1cb005f9836a5a2
Aiswarya <ee23bt055@gmail.com>
2025-01-31 19:19:29 +0530

    haha

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+// import About from './pages/About';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/about" element={<About />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Front-end/src/Components/Home.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+    return <h1>Welcome to Home Page 🏠</h1>;
+  }
+  
